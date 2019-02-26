ab312857d0286dfbfb6edc362f9e4751e02389b5
Felipe Macías <felipemacias@Felipes-MacBook-Pro.local>
2019-02-25 20:06:55 -0500

    display articles in descending order

--- main.py
+++ main.py
@@ -2,32 +2,28 @@ import pandas as pd
 import glob
 from time import time
 
+word = input("Type the word: ") 
 tiempo_inicial = time()
 #iloc: Select row by index label.
 columns = ['id', 'title', 'content']
 allFiles = glob.glob("*.csv")
 frame = pd.DataFrame()
 list_ = []
+dictionary = {}
 for file_ in allFiles:
     df = pd.read_csv(file_,index_col=None, header=0, usecols=columns)
     list_.append(df)
 frame = pd.concat(list_)
-word = input("Type the word: ") 
 for i,content in enumerate(frame['content']):
     count = 0
     for article in content.split():
         if word == article:
             count+=1
     if count > 0:
-        print(count, frame['id'].iloc[i],frame['title'].iloc[i])
-
-    
-
-
-
-
-
-
+        dictionary[frame['id'].iloc[i]] = count, frame['title'].iloc[i] #adding to a hashmap
+sortedList = sorted(dictionary.items(), key=lambda kv: kv[1][0], reverse=True) #sorting list in descending order
+for i in range(10):
+    print(sortedList[i])
 
 tiempo_final = time()
 tiempo_ejecucion = tiempo_final - tiempo_inicial
