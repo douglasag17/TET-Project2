88696d5461390e2386745140e51762a58a614e1c
Felipe Macías <felipemacias@Felipes-MacBook-Pro.local>
2019-03-04 16:45:59 -0500

    Reading all files

--- secuencial.cpp
+++ secuencial.cpp
@@ -11,68 +11,94 @@
 
 using namespace std;
 
-int main() {
+vector<vector<string>> readFiles(string fileP){
     string line;
-    vector<vector<string> > lines;
-    multimap<string, string> dictionary; 
-    ifstream file("all-the-news/articles1.csv");
-    if (file) {
-        while (getline(file, line)) {
+    vector<vector<string>> lines;
+    ifstream file(fileP);
+    if (file)
+    {
+        while (getline(file, line))
+        {
             size_t n = lines.size();
             lines.resize(n + 1);
             istringstream ss(line);
             string field, push_field("");
             bool no_quotes = true;
-            while (getline(ss, field, ',')) {
-                if (static_cast<size_t>(count(field.begin(), field.end(), '"')) % 2 != 0) {
+            while (getline(ss, field, ','))
+            {
+                if (static_cast<size_t>(count(field.begin(), field.end(), '"')) % 2 != 0)
+                {
                     no_quotes = !no_quotes;
                 }
                 push_field += field + (no_quotes ? "" : ",");
-                if (no_quotes) {
+                if (no_quotes)
+                {
                     lines[n].push_back(push_field);
                     push_field.clear();
                 }
             }
         }
     }
+    return lines;
+}
+
+string toLower(string wordP)
+{
+    string word = wordP;
+    for (int i = 0; i < wordP.length(); i++)
+    {
+        word[i] = tolower(wordP[i]);
+    }
+    return word;
+}
+
+int main()
+{
+    multimap<int, string, greater<int>> dictionary;
     string word;
     cout << "Ingrese una palabra: ";
     cin >> word;
-    string wordUpper = word;
-    string wordLower = word;
-    int j = 0;
-    for(int i = 0; i < word.length(); i++) {
-        wordUpper[i] = toupper(word[i]);
-        wordLower[j] = tolower(word[j]);
-        j++;
-    }
-    for (auto line : lines) {
-        int length = line.size();
-        if(length > 4) {
-            replace(line[length-1].begin(), line[length-1].end(), ',', ' ');
-            replace(line[length-1].begin(), line[length-1].end(), '.', ' ');
-            replace(line[length-1].begin(), line[length-1].end(), ';', ' ');
-            replace(line[length-1].begin(), line[length-1].end(), ':', ' ');
-            replace(line[length-1].begin(), line[length-1].end(), '?', ' ');
-            replace(line[length-1].begin(), line[length-1].end(), '!', ' ');
-            replace(line[length-1].begin(), line[length-1].end(), '"', ' ');
-            stringstream ss(line[length-1]);
-            string token;
-            int count = 0;
-            while (getline(ss, token, ' ')) {
-                if(token.compare(wordLower) == 0 || token.compare(wordUpper) == 0){
-                    count++;
+    word = toLower(word);
+    vector<string> files = {"articles1.csv", "articles2.csv", "articles3.csv"};
+    for (int i = 0; i < 3; ++i)
+    {
+        vector<vector<string>> lines = readFiles(files[i]);
+        for (auto line : lines)
+        {
+            int length = line.size();
+            if (length > 4)
+            {
+                replace(line[length - 1].begin(), line[length - 1].end(), ',', ' ');
+                replace(line[length - 1].begin(), line[length - 1].end(), '.', ' ');
+                replace(line[length - 1].begin(), line[length - 1].end(), ';', ' ');
+                replace(line[length - 1].begin(), line[length - 1].end(), ':', ' ');
+                replace(line[length - 1].begin(), line[length - 1].end(), '?', ' ');
+                replace(line[length - 1].begin(), line[length - 1].end(), '!', ' ');
+                replace(line[length - 1].begin(), line[length - 1].end(), '"', ' ');
+                stringstream ss(line[length - 1]);
+                string token;
+                int count = 0;
+                while (getline(ss, token, ' '))
+                {
+                    if (toLower(token).compare(word) == 0)
+                    {
+                        count++;
+                    }
                 }
-            }
-            if (count > 0) {
-                dictionary.insert(pair<string, string>(to_string(count) + "\t" + line[1], line[2])); // adding to a multimap
+                if (count > 0)
+                {
+                    dictionary.insert(pair<int, string>(count, line[1] + "\t" + line[2])); // adding to a multimap
+                }
             }
         }
     }
     // printing multimap dictionary
-    multimap<string, string>::iterator itr;
-    for (itr = dictionary.begin(); itr != dictionary.end(); ++itr) { 
-        cout << itr->first << '\t' << itr->second << '\n'; 
+    multimap<int, string>::iterator itr;
+    int i = 0;
+    for (itr = dictionary.begin(); itr != dictionary.end(); ++itr) {
+        cout << itr->first << '\t' << itr->second << endl;
+        if(i == 9) break;
+        i++;
     }
     return 0;
 }
