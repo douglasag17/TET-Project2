a3b6a25afe6ec1a28170cdd9e27122b4d07fd84e
Andres Felipe Avendaño Restrepo <aavenda1@hdpmaster.dis.eafit.edu.co>
2019-03-14 15:57:57 -0500

    mpi all csv

--- pcam.cpp
+++ pcam.cpp
@@ -11,7 +11,7 @@
 #include <string.h>
 #include "mpi.h"
 #include <omp.h>
-#define N 100
+#define N 110
 
 using namespace std;
 
@@ -39,6 +39,8 @@ string toLower(string wordP) {
     char valueRec[N];
     char vectorKeyRec[N];
     char vectorValRec[N];
+    char vectorKeyRec2[N];
+    char vectorValRec2[N];
 
     if (rank == 0) {
       multimap<string, vector<pair<int,string>>> dictionary;
@@ -79,9 +81,10 @@ string toLower(string wordP) {
       string word;
       //do{
         vector<pair<int,string>> vectorFinal;
-        int i, total = 0;
+	int i, total = 0;
         cout << "Enter the word (/ to quit): ";
         cin >> word;
+	
         word = toLower(word);
         const char *wordSend = word.c_str();
         MPI_Send(wordSend, strlen(wordSend), MPI_CHAR, 1, 0, MPI_COMM_WORLD);
@@ -97,11 +100,17 @@ string toLower(string wordP) {
 	    //total += itAux->second[i].first;
 	  }
 	  //cout << word << " is " << total << " times in all the news." << endl;
-	  for (int i = 0; i < 10; i++) {
-	    MPI_Recv(&vectorKeyRec, N, MPI_CHAR, 1, 1, MPI_COMM_WORLD, &info);
-	    MPI_Recv(&vectorValRec, N, MPI_CHAR, 1, 1, MPI_COMM_WORLD, &info);
+	  for (int i = 0; i < 20; i++) {
+	    MPI_Recv(&vectorKeyRec, N, MPI_CHAR, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &info);
+	    MPI_Recv(&vectorValRec, N, MPI_CHAR, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &info);
 	    vectorFinal.push_back(make_pair(atoi(vectorKeyRec), vectorValRec));
-	  } 
+	  }
+	  cout<<"HOLIIIIIIIIIIIII"<<endl;
+	  /*for (int i = 0; i < 10; i++) {
+            MPI_Recv(&vectorKeyRec2, N, MPI_CHAR, 2, 2, MPI_COMM_WORLD, &info);
+            MPI_Recv(&vectorValRec2, N, MPI_CHAR, 2, 2, MPI_COMM_WORLD, &info);
+            vectorFinal.push_back(make_pair(atoi(vectorKeyRec2), vectorValRec2));
+	    }*/ 
         }else{
 	  //cout << word << " not found." << endl;
         }
@@ -110,8 +119,7 @@ string toLower(string wordP) {
 	  cout << vectorFinal[i].first << " " << vectorFinal[i].second << endl;
 	}
 	//}while(word != "/");
-    }
-    
+    }    
     if (rank == 1) {
       multimap<string, vector<pair<int,string>>> dictionary;
       multimap<string, vector<pair<int,string>>>::iterator it;
@@ -174,8 +182,64 @@ string toLower(string wordP) {
     }
 
     if (rank == 2) {
-      MPI_Recv(&valueRec, 40, MPI_CHAR, 0, 0, MPI_COMM_WORLD, &info);
-      //cout << valueRec << endl;
+      multimap<string, vector<pair<int,string>>> dictionary;
+      multimap<string, vector<pair<int,string>>>::iterator it;
+      vector<string> files = {"Particles3.csv"};
+      for (int i = 0; i < files.size(); ++i){
+        ifstream file(files[i]);
+        string pos;
+        string id;
+        string title;
+        string content;
+        if (file){
+          while (getline(file, pos, '\t')){
+            getline(file, id, '\t');
+            getline(file, title, '\t');
+            getline(file, content);
+            stringstream ss(content);
+            string token;
+            while (getline(ss, token, ' '))
+              {
+                vector<pair<int,string>> article;
+                it = dictionary.find(token);
+                if(it == dictionary.end()){
+                  article.push_back(make_pair(1, id + "," + title));
+                  dictionary.insert(make_pair(token, article));
+                }else{
+                  if(it->second.back().second.compare(id + "," + title) == 0){
+                    it->second.back().first += 1;
+                  }else{
+                    it->second.push_back(make_pair(1, id + "," + title));
+                  }
+                }
+              }
+          }
+        }
+      }
+      multimap<string, vector<pair<int,string>>>::iterator itAux;
+      string word;
+      int i, total = 0;
+      MPI_Recv(&valueRec, N, MPI_CHAR, 0, 0, MPI_COMM_WORLD, &info);
+      word = valueRec;
+      itAux = dictionary.find(word);
+      if(itAux != dictionary.end()){
+        sort(itAux->second.begin(), itAux->second.end(), sortinrev);
+        for (i = 0; i < itAux->second.size(); ++i){
+          if(i < 10){
+            // cout << itAux->second[i].first << " "<< itAux->second[i].second << endl;                                                                                              
+            string key = to_string(itAux->second[i].first);
+            const char *keySend = key.c_str();
+            string value = itAux->second[i].second;
+            const char *valueSend = value.c_str();
+            MPI_Send(keySend, strlen(keySend), MPI_CHAR, 0, 1, MPI_COMM_WORLD);
+            MPI_Send(valueSend, strlen(valueSend), MPI_CHAR, 0, 1, MPI_COMM_WORLD);
+          }
+          total += itAux->second[i].first;
+        }
+        //cout << word << " is " << total << " times in all the news." << endl;                                                                                                      
+      }else{
+        //cout << word <<" not found." << endl;                                                                                                                                      
+      }
     }
 
     MPI_Finalize();
